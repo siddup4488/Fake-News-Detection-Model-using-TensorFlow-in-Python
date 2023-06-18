f9448fe1abfe87f342cc8dc08df7c9206f989734
Prathapani Santhosh Krishna Siddhardha <61466844+siddup4488@users.noreply.github.com>
2023-06-18 19:17:22 +0530

    Update Data Encoding.py

--- Data Encoding.py
+++ Data Encoding.py
@@ -2,3 +2,31 @@
 le = preprocessing.LabelEncoder()
 le.fit(data['label'])
 data['label'] = le.transform(data['label'])
+embedding_dim = 50
+max_length = 54
+trunc_type = 'post'
+padding_type = 'post'
+oov_tok = "<OOV>"
+training_size = 3000
+test_portion = .1
+
+
+title = []
+text = []
+labels = []
+for x in range(training_size):
+	title.append(data['title'][x])
+	text.append(data['text'][x])
+	labels.append(data['label'][x])
+tokenizer1 = Tokenizer()
+tokenizer1.fit_on_texts(title)
+word_index1 = tokenizer1.word_index
+vocab_size1 = len(word_index1)
+sequences1 = tokenizer1.texts_to_sequences(title)
+padded1 = pad_sequences(
+	sequences1, padding=padding_type, truncating=trunc_type)
+split = int(test_portion * training_size)
+training_sequences1 = padded1[split:training_size]
+test_sequences1 = padded1[0:split]
+test_labels = labels[0:split]
+training_labels = labels[split:training_size]
